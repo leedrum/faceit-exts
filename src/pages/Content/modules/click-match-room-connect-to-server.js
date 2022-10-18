4ca9541a081d2326544d3115aeb7022f4955d960
leedrum <levantungat@gmail.com>
2022-10-18 22:08:42 +0700

    add auto connect to server

---
+++ src/pages/Content/modules/click-match-room-connect-to-server.js
@@ -0,0 +1,50 @@
+import { getRoomId } from '../../../helpers/match-room'
+import {
+  hasFeatureAttribute,
+  setFeatureAttribute
+} from '../../../helpers/dom-element'
+import { getSelf } from '../../../helpers/faceit-api'
+import { getSetting, saveSetting } from '../../../helpers/settings'
+
+const FEATURE_ATTRIBUTE = 'connect-to-server'
+const DELAY = 5000
+
+export const ClickMatchRoomConnectToServer = async () => {
+  const goToServerElement = document.getElementById("parasite-container").shadowRoot.querySelectorAll('[rel="noopener noreferrer"]')[0]
+
+  if (!goToServerElement) {
+    return
+  }
+
+  if (goToServerElement.textContent !== "Connect") {
+    return
+  }
+
+  const self = await getSelf()
+  const roomId = getRoomId()
+  const isSelfInMatch = document.getElementById("parasite-container").shadowRoot.textContent.indexOf(self.nickname) > -1
+
+  if (!isSelfInMatch) {
+    return
+  }
+
+  if (hasFeatureAttribute(FEATURE_ATTRIBUTE, goToServerElement)) {
+    return
+  }
+
+  setFeatureAttribute(FEATURE_ATTRIBUTE, goToServerElement)
+
+  const matchRoomLastConnectToServer = await getSetting("matchRoomLastConnectToServer")
+
+  if (matchRoomLastConnectToServer === roomId) {
+    return
+  }
+
+  await saveSetting({ matchRoomLastConnectToServer: roomId })
+
+  setTimeout(() => {
+    goToServerElement.click()
+  }, DELAY)
+}
+
+export default ClickMatchRoomConnectToServer
