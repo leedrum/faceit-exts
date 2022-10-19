28a39e1abd4a1fa6b249e9455624f1e8495ee67d
leedrum <levantungat@gmail.com>
2022-10-19 15:23:45 +0700

    update version

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -22,7 +22,7 @@ export const AboutComponent = () => {
       <Row>
         <Grid item xs={12}>
           <Typography component={'p'}>
-            Version: 1.1.1
+            Version: 1.1.2
           </Typography>
         </Grid>
       </Row>
